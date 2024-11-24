c93ce4db45e530fd486b0d3d673e5c9ea1dec08c
SriAlanyaReddy <164356102+SriAlanyaReddy@users.noreply.github.com>
2024-11-24 20:51:17 +0530

    [1342] [Time Beats: 100.00%] [Memory Beats: 21.36%] - LeetPush

---
+++ day10/1342-Number-of-Steps-to-Reduce-a-Number-to-Zero.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int numberOfSteps(int n) {
+        if(n==0){
+            return 0;
+        }
+        return (n%2==0)?1+numberOfSteps(n/2):1+numberOfSteps(n-1);
+    }
+}
